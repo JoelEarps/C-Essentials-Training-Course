68de3d54ce480f87f36be6c17438181a394d3916
Joel Earps <joeldearps@gmail.com>
2020-12-18 17:38:01 +0000

    chapter 2 lesson 2

--- Ex_Files_CPlusPlus_EssT/Exercise Files/Chap02/hello.cpp
+++ Ex_Files_CPlusPlus_EssT/Exercise Files/Chap02/hello.cpp
@@ -1,9 +1,16 @@
 // hello.cpp by Bill Weinman <http://bw.org/>
-#include <cstdio>
-using namespace std;
+#include <cstdio> //not a c++ statement, this is a directive to the preprcoessor
+//can also use iostream - functions like cout
+using namespace std; // shortcut to include namespaces - easier to use std library
 
-int main()
+int main() //used to be reuqired to have int argc, char **arg
+//modern c++ programs dont have anything in ()
 {
-    puts("Hello, World!");
+    //body of function
+    puts("Hello, World!"); // puts - pushes string to console
     return 0;
 }
+
+//blank spaces ignored c++
+//compiler doesnt require this format, easier for programmer
+// \n represents a new line
